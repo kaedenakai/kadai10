8be343e3fa57ea614722fe8b323325ef6f7830e0
kaedenakai <kaedenakai1115@gmail.com>
2024-09-22 16:05:26 +0900

    UPDATE

--- retro/index.php
+++ retro/index.php
@@ -13,7 +13,7 @@ sschk();
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
-  <title>レトロスペクティブ大全</title>
+  <title>ホーム</title>
   <link href="css/bootstrap.min.css" rel="stylesheet">
   <style>div{padding: 10px;font-size:16px;}</style>
 </head>
@@ -26,6 +26,7 @@ sschk();
       <?=$_SESSION["name"]?>さん、こんにちは！
       <?php if($_SESSION["kanri_flg"]=="1"){ ?>
         <a class="navbar-brand" href="user.php">ユーザー登録</a>
+        <a class="navbar-brand" href="user_select.php">ユーザー一覧</a>
       <?php } ?>
       <a class="navbar-brand" href="logout.php">ログアウト</a>
       </div>
